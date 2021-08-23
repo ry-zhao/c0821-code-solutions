5552c9fa313b950328304e6d5ddbb92a43d9049a
Ryan Zhao <ryanzhao21@gmail.com>
2021-08-23 10:39:56 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Ryan Zhao';
+var isCool = true;
+var totalPets = 0;
+var unknown = null;
+var never;
+console.log('Full name:', fullName);
+console.log('fullName type:', typeof fullName);
+console.log('Is cool:', isCool);
+console.log('isCool type:', typeof isCool);
+console.log('Total pets:', totalPets);
+console.log('totalPets type:', typeof totalPets);
+console.log('unknown:', unknown);
+console.log('unkown type:', typeof unknown);
+console.log('never:', never);
+console.log('never type:', typeof never);
