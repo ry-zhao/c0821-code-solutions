ae1939a16f00c8823012163dd8d771987b6a0d01
Ryan Zhao <ryanzhao21@gmail.com>
2021-08-24 11:05:34 -0700

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,73 @@
+var orderHistory = [
+  {
+    items: [
+      {
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephanie',
+        rWindowClose: '08/04/20',
+        price: 33.99,
+        support: false
+      },
+      {
+        name: 'GameCube Controller',
+        author: null,
+        rWindowClose: '08/04/20',
+        price: 94.95,
+        support: true
+      }
+    ],
+    orderPlaced: '07/03/20',
+    delivered: '07/05/20',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNum: '113-2883177-2648248'
+  },
+  {
+    items: [
+      {
+        name: 'Gamecube Controller Adapter',
+        author: null,
+        rWindowClose: '08/05/20',
+        price: 15.98,
+        support: false
+      }
+    ],
+    orderPlaced: '07/04/20',
+    delivered: '07/07/20',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNum: '114-2875557-9059409'
+  },
+  {
+    items: [
+      {
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        rWindowClose: '08/19/20',
+        price: 41.33,
+        support: false
+      }
+    ],
+    orderPlaced: '07/19/20',
+    delivered: '07/20/20',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNum: '113-9984268-1280257'
+  },
+  {
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        rWindowClose: '09/07/20',
+        price: 31.55,
+        support: false
+      }
+    ],
+    orderPlaced: '08/04/20',
+    delivered: '08/08/20',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    orderNum: '114-3941689-8772232'
+  }
+];
