60e3801bd90525c6dc4ac79618f35a27dcca57c7
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-01 17:11:48 -0700

    In progress.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,25 @@
 /* exported unique */
+//  unique
+//  input: Array array
+//  output: Array containing only the unique elements of array
+
+//  PSEUDOCODE
+//  create storage for output
+//  create and store Object counter
+//  traverse array
+//    if element doesn't exist in counter
+//      append element to counter with value true
+//      push element to output
+//  return output
+
+function unique(array) {
+  var output = [];
+  var counter = {};
+  for (var i = 0; i < array.length; i++) {
+    if (!counter[array[i]]) {
+      counter[array[i]] = true;
+      output.push(array[i]);
+    }
+  }
+  return output;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,36 @@
 /* exported zip */
+//  zip
+//  input: Arrays first and second
+//  output: Array of Arrays with each subArray containing an element from first and second
+//  from the same index
+
+//  PSEUDOCODE
+//  declare var length
+//  check if first is longer than second
+//    if so, assign length of second to length
+//  else,
+//    assign length of first to length
+//  create storage for output
+//  traverse arrays
+//    create and store new array temp
+//    push element from first to temp
+//    push element from second to temp
+//    push temp to output
+//  return output
+
+function zip(first, second) {
+  var length;
+  if (first.length > second.length) {
+    length = second.length;
+  } else {
+    length = first.length;
+  }
+  var output = [];
+  for (var i = 0; i < length; i++) {
+    var temp = [];
+    temp.push(first[i]);
+    temp.push(second[i]);
+    output.push(temp);
+  }
+  return output;
+}
