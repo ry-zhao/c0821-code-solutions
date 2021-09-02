095b19a047048515e11ce9b7fa04638fb204f2ab
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-02 16:25:50 -0700

    In progress.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -7,24 +7,41 @@
 //  PSEUDOCODE:
 //  create storage for output
 //  traverse first array
+//    create and store Boolean isDifferent and set to true
 //    traverse second array
-//      check if element of first array is equal to element
-//        if so, break
-//      if not, append element of first array to output
+//      if element of first array equals element
+//        set isDifferent to false
+//        break
+//    if isDifferent is true
+//      append to output
 //  traverse second array
-//    traverse first array
-//      check if element exists in unique OR of second array is equal to element
-//        if so, break
-//      if not, append element of second array to output
+//    repeat the same as above but with arrays switched
 //  return output
 
 function difference(first, second) {
   var output = [];
   for (var i = 0; i < first.length; i++) {
+    var isDifferentFromSecond = true;
     for (var j = 0; j < second.length; j++) {
-      if (first[i] !== second[j]) {
-        output.push(first[i]);
+      if (first[i] === second[j]) {
+        isDifferentFromSecond = false;
+        break;
       }
+    }
+    if (isDifferentFromSecond === true) {
+      output.push(first[i]);
+    }
+  }
+  for (var k = 0; k < second.length; k++) {
+    var isDifferentFromFirst = true;
+    for (var l = 0; l < first.length; l++) {
+      if (second[k] === first[l]) {
+        isDifferentFromFirst = false;
+        break;
+      }
+    }
+    if (isDifferentFromFirst === true) {
+      output.push(second[k]);
     }
   }
   return output;
