ed2923b7d6dfc361f297a08a55ac3fb1cd038f7d
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-03 16:41:01 -0700

    Added additional enhancements.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,13 +1,21 @@
 var $allSpan = document.querySelectorAll('span');
+var $dimmer = document.querySelector('.dimmer');
+var $h2 = document.querySelector('h2');
+var $button = document.querySelector('button');
 var i = 0;
 var str = 'grumpy wizards make toxic brew';
+var hit = 0;
+var miss = 0;
 
 //  PSEUDOCODE - evaluate
 //  check if i is within range of str
 //    -if so, check if event.key matches str at i
 //      -if so, assign $allSpan at i class 'correct'
 //      -increment i
-//      -assign $allSpan at i class 'current'
+//      -if i is not out of range
+//        -assign $allSpan at i class 'current'
+//      -else
+//        -unhide dimmer
 //    -else
 //      -assign $allSpan at i class 'incorrect'
 
@@ -16,11 +24,31 @@ function evaluate(event) {
     if (event.key === str[i]) {
       $allSpan[i].className = 'correct';
       i++;
-      $allSpan[i].className = 'current';
+      hit++;
+      if (i < str.length) {
+        $allSpan[i].className = 'current';
+      } else {
+        $dimmer.className = 'dimmer container';
+        $h2.textContent = 'Your accuracy: ' +
+         (hit / (hit + miss)).toPrecision(4) * 100 + '%';
+      }
     } else {
       $allSpan[i].className = 'incorrect';
+      miss++;
     }
   }
 }
 
+function reset() {
+  i = 0;
+  hit = 0;
+  miss = 0;
+  $dimmer.className = 'dimmer container hide';
+  $allSpan[0].className = 'current';
+  for (var j = 1; j < $allSpan.length; j++) {
+    $allSpan[j].className = '';
+  }
+}
+
 document.addEventListener('keypress', evaluate);
+$button.addEventListener('click', reset);
