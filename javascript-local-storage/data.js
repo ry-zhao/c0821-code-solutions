3188f18381c14e187341ec1d518bc99dbba59fde
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-03 13:06:25 -0700

    Completed javascript-local-storage exercise.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,15 @@
+/* exported todos */
+
+var todos = [];
+
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+function stringifyAndStoreTodos() {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
+
+window.addEventListener('beforeunload', stringifyAndStoreTodos);
