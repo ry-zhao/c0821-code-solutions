a52091e886ba57c9d26d336e1a0e1fac60dbdbbd
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-03 00:13:16 -0700

    Completed array-manipulation-3 challenge.

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,21 @@
 /* exported intersection */
+//  intersection
+//  input: Arrays first, second
+//  output: Array containing intersection
+
+//  PSEUDOCODE
+//  create storage for output
+//  traverse first
+//    check if element appears in second
+//      if so, append to output
+//  return output
+
+function intersection(first, second) {
+  var output = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) !== -1) {
+      output.push(first[i]);
+    }
+  }
+  return output;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,27 @@
 /* exported union */
+
+//  union
+//  input: Arrays first, second
+//  output: Array with union of two inputs
+
+//  PSEUDOCODE
+//  create storage for output
+//  traverse first array
+//    push element to output
+//  traverse second array
+//    check if element exists in output
+//      if not, push element to output
+//  return output
+
+function union(first, second) {
+  var output = [];
+  for (var i = 0; i < first.length; i++) {
+    output.push(first[i]);
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (output.indexOf(second[j]) === -1) {
+      output.push(second[j]);
+    }
+  }
+  return output;
+}
