2f1e1869ada4aac3411362338947b976e564805e
Ryan Zhao <ryanzhao21@gmail.com>
2021-09-14 11:47:34 -0700

    Completed javascript-this exercise.

---
+++ javascript-this/student.js
@@ -0,0 +1,13 @@
+/* exported student */
+
+var student = {
+  firstName: 'Ryan',
+  lastName: 'Zhao',
+  subject: 'JavaScript',
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.getFullName() + ' and I am studying ' + this.subject + '.';
+  }
+};
