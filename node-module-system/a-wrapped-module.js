a5f3ff6c66aac95fed656f0c9c26c7160e7dfb96
ry-zhao <ryanzhao21@gmail.com>
2021-10-05 17:54:18 -0700

    Completed node-module-system exercise.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log(`typeof exports: ${typeof exports}`);
+console.log(`typeof require: ${typeof require}`);
+console.log(`typeof module: ${typeof module}`);
+console.log(`typeof __filename: ${typeof __filename}`);
+console.log(`typeof __dirname: ${typeof __dirname}`);
