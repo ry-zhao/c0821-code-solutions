bb1d0237e8ce6923e654f9f2add6517e4edd4f36
ry-zhao <ryanzhao21@gmail.com>
2021-10-10 16:35:44 -0700

    Refactored cat.js.

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-let output = [];
-const i = 2;
+let output = '';
 
 const cat = i => fs.readFile(`./${process.argv[i]}`, 'utf-8', (err, data) => {
   if (err) throw err;
-  output += data + '\n';
+  output += data;
   if (i === process.argv.length - 1) {
     console.log(output);
   } else {
+    output += '\n';
     cat(++i);
   }
 });
 
-cat(i);
+cat(2);
