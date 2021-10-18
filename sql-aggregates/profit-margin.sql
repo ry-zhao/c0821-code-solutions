db797ffc8c7175991574bfeef160ca14c85b4eeb
ry-zhao <ryanzhao21@gmail.com>
2021-10-18 10:04:17 -0700

    profit-margin.sql in progress.

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,5 +1,15 @@
-select "films"."title",
-       sum("films"."replacementCost") as "cost"
-  from "inventory"
+with "filmCosts" as (
+  select "films"."title",
+       sum("films"."replacementCost") from "inventory"
   join "films" using ("filmId")
-  group by "films"."title"
+  group by ("filmId")
+)
+-- with "filmRev" as (
+--   select "films"."title",
+--          sum("payments"."amount") from "payments"
+--   join "rentals" using ("rentalId")
+--   join "inventory" using ("inventoryId")
+--   join "films" using ("filmId")
+--   group by ("filmId")
+-- )
+select * from "filmCosts"
