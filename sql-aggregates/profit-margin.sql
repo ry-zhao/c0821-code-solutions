0189b611304c0ae5b5e9f5d917f7e299437311f4
ry-zhao <ryanzhao21@gmail.com>
2021-10-20 21:56:01 -0700

    Completed profit-margin.sql.

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,15 +1,20 @@
-with "filmCosts" as (
-  select "films"."title",
-       sum("films"."replacementCost") from "inventory"
+with "filmCost" as (
+  select "films"."filmId",
+         "films"."title",
+         sum("films"."replacementCost") as "cost" from "inventory"
   join "films" using ("filmId")
   group by ("filmId")
-)
--- with "filmRev" as (
---   select "films"."title",
---          sum("payments"."amount") from "payments"
---   join "rentals" using ("rentalId")
---   join "inventory" using ("inventoryId")
---   join "films" using ("filmId")
---   group by ("filmId")
--- )
-select * from "filmCosts"
+  ),"filmRev" as (
+    select "films"."filmId",
+           "films"."title",
+           sum("payments"."amount") as "rev" from "payments"
+    join "rentals" using ("rentalId")
+    join "inventory" using ("inventoryId")
+    join "films" using ("filmId")
+    group by ("filmId")
+  )
+select "films"."title",
+       "filmRev"."rev" - "filmCost"."cost" as "profit" from "films"
+  join "filmCost" using ("filmId")
+  join "filmRev" using ("filmId")
+  order by ("profit") desc;
