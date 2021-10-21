6c0bdb8846db427f9382bc0e610b5fecbe88fdc4
ry-zhao <ryanzhao21@gmail.com>
2021-10-21 16:30:10 -0700

    Completed react-form-controls exercise.

---
+++ react-form-controls/src/NewsletterForm.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class NewsletterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" onChange={this.handleChange}></input>
+        <button onClick={this.handleSubmit}>Sign Up</button>
+      </form>
+    );
+  }
+
+  handleChange() {
+    this.setState(prevState => (
+      { value: event.target.value }
+    ));
+  }
+
+  handleSubmit() {
+    console.log(this.state);
+    event.preventDefault();
+  }
+}
+
+export { NewsletterForm as default };
---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NewsletterForm from './NewsletterForm.jsx';
+
+const element = <NewsletterForm />;
+
+ReactDOM.render(element, document.getElementById('root'));
