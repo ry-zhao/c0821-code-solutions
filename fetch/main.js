637babd58dfc49191259043ab43990b9c06b4ef6
ry-zhao <ryanzhao21@gmail.com>
2021-10-25 13:17:59 -0700

    Added catch.

--- fetch/main.js
+++ fetch/main.js
@@ -1,3 +1,4 @@
 fetch('https://pokeapi.co/api/v2/pokemon/slowking/')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(err => console.error('Fetch failed!', err));
