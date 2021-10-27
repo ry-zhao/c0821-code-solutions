0b1ac9526fd1114271bc943edc5a936eb278406b
ry-zhao <ryanzhao21@gmail.com>
2021-10-27 14:50:51 -0700

    Refactored code to throw ClientError on line 7 of authorization-middleware.js.

--- api-authorization/server/authorization-middleware.js
+++ api-authorization/server/authorization-middleware.js
@@ -4,7 +4,7 @@ const ClientError = require('./client-error'); // eslint-disable-line
 function authorizationMiddleware(req, res, next) {
   const token = req.headers['x-access-token'];
   if (!token) {
-    res.status(401).json({ error: 'authentication required' });
+    throw new ClientError(401, 'authentication required');
   }
   req.user = jwt.verify(token, process.env.TOKEN_SECRET);
   next();
