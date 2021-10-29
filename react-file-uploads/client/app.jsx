eee6c3cf1f82aeeee6926bd6cc14f70115414515
ry-zhao <ryanzhao21@gmail.com>
2021-10-29 00:28:59 -0700

    Removed comments.

--- react-file-uploads/client/app.jsx
+++ react-file-uploads/client/app.jsx
@@ -16,35 +16,6 @@ export default class App extends React.Component {
   }
 
   handleSubmit(event) {
-    /**
-     * Prevent the browser's default behavior for form submissions.
-     *
-     * Create a `new` FormData object.
-     *
-     * Append two entries to the form data object you created.
-     *   1. "caption" the value of this.state.caption
-     *   2. "image" the value of this.fileInputRef.current.files[0]
-     *
-     * Use fetch() to send a POST request to /api/uploads. The body
-     * should be the form data object you created (not a JSON string)
-     * Headers are not necessary as fetch will use the correct Content-Type
-     * automatically (multiplart/form-data).
-     *
-     * Then 😉,
-     *   parse the JSON response body
-     * Then 😉,
-     *   log the parsed response body
-     *   set the caption state back to an empty string
-     *   assign null to this.fileInputRef.current.value to clear the file
-     * Catch any error in the promise chain.
-     *
-     * References:
-     * https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData
-     * https://developer.mozilla.org/en-US/docs/Web/API/FormData/append
-     * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_a_file
-     * https://reactjs.org/docs/uncontrolled-components.html#the-file-input-tag
-     * https://reactjs.org/docs/refs-and-the-dom.html
-     */
     event.preventDefault();
     const formData = new FormData();
     formData.append('caption', this.state.caption);
